2ce955552b9fa606827caf729db37d8793aabde6
Yara Castro <yaracastro24@gmail.com>
2019-05-28 22:37:46 -0500

    questions answered

--- assignments/this.js
+++ assignments/this.js
@@ -1,16 +1,16 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Call- we use it to call our parameters one by one. 
+* 2. Apply - we apply this keyword when we want to take arguments as an array. 
+* 3. Bind - 
+* 4. New - Creates a new object and set the constructor of an object to another object.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+console.log(this);
 // code example for Window Binding
 
 // Principle 2
